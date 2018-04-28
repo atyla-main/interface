b872fc9058591e70749c2d81d093f921f6e6d0c6
maisonbleue <vincent.trastour@lecollectionist.com>
2018-04-28 23:05:39 +0200

    ATYLA-APIV1 remove unused package node-sass-middleware

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 var api = require('./routes/api')
 
 var indexRouter = require('./routes/index');
@@ -40,12 +39,6 @@ app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/login', async function(req, res) {
