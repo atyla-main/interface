74656a89d7ee251422e5d7fae6c8b00a52d823d2
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-03 01:09:01 +0200

    HEROKU setup

--- models/index.js
+++ models/index.js
@@ -8,17 +8,7 @@ var db        = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  var match = process.env[config.use_env_variable].match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
-  sequelize = new Sequelize(match[5], match[1], match[2], {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging: false,
-      dialectOptions: {
-          ssl: true
-      }
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
