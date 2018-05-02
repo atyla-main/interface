3a1c38ff68c5e45c19e750148a9d256349e7961a
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-03 00:44:11 +0200

    HEROKU setup

--- app.js
+++ app.js
@@ -80,19 +80,22 @@ app.use('/api', passport.authenticate('jwt', { session: false }), api);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
-});
+if ('development' === app.get('env')) {
+  app.use(function(req, res, next) {
+	  next(createError(404));
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
+  // error handler
+  app.use(function(err, req, res, next) {
+	  // set locals, only providing error in development
+	  res.locals.message = err.message;
+	  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	  // render the error page
+	  res.status(err.status || 500);
+	  res.render('error');
+  });
+}
 
 module.exports = app;
