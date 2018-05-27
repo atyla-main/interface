a120c62e4cf53c286b16e0f3a7f0a893035e9312
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-27 18:12:52 +0200

    API-6 user add new attributes and validation email, unique, date, isIn

--- views/users.js
+++ views/users.js
@@ -4,7 +4,20 @@ function attributes(user) {
     lastName: user.lastName,
     email: user.email,
     password: user.password,
-    token: user.token
+    token: user.token,
+    source: user.source,
+    stage: user.stage,
+    gender: user.gender,
+    birthDate: user.birthDate,
+    birthCity: user.birthCity,
+    phone: user.phone,
+    citizenship: user.citizenship,
+    country: user.country,
+    address: user.address,
+    zipCode: user.zipCode,
+    city: user.city,
+    state: user.state,
+    optIn: user.optIn
   }
 }
 
