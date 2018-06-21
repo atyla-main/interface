1ca588185a2eb165fb3dde82a4f3c5dfa40c8215
maisonbleue <vincent.trastour@lecollectionist.com>
2018-06-21 12:49:53 +0200

    API-8 prospects table model and crud

--- views/index.js
+++ views/index.js
@@ -3,11 +3,13 @@ const users = require('./users');
 const newsletters = require('./newsletters');
 const icos = require('./icos');
 const orders = require('./orders');
+const prospects = require('./prospects');
 
 module.exports = {
 	roles,
 	users,
 	newsletters,
 	icos,
-  orders
+  orders,
+  prospects
 };
