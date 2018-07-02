b34a8c2902ed4539cb64eeeea7334eed91cf702c
maisonbleue <vincent.trastour@lecollectionist.com>
2018-07-02 21:02:22 +0200

    API-10-UUID cleaning code

--- models/user.js
+++ models/user.js
@@ -1,30 +1,38 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-	var User = sequelize.define(
-		'User',
-		{
+  var User = sequelize.define(
+    'User',
+    {
       uuid: {
         type: DataTypes.UUID,
         primaryKey: true,
         defaultValue: DataTypes.UUIDV4
       },
-			firstName: {
-				type: DataTypes.STRING,
-				allowNull: false,
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
         validate: {
           notEmpty: true
         }
-			},
-			lastName: {
-				type: DataTypes.STRING,
-				allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
         validate: {
           notEmpty: true
         }
-			},
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
         validate: {
           isEmail: true,
           isUnique(value, next) {
@@ -39,97 +47,90 @@ module.exports = (sequelize, DataTypes) => {
             });
           }
         },
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-        validate: {
-          notEmpty: true
-        }
-			},
-			token: {
-				type: DataTypes.STRING,
-				allowNull: true
-			},
+      },
+      token: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
       source: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       stage: {
-				type: DataTypes.STRING,
-				allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
         validate: {
           isIn: [['inscription', 'confirmation_send', 'confirmed', 'kyc_send', 'validated', 'denied', 'unsubscribed']]
         }
       },
       gender: {
-				type: DataTypes.STRING,
-				allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
         validate: {
           isIn: [['mr', 'mrs']]
         }
       },
       birthDate: {
-				type: DataTypes.DATE,
-				allowNull: true,
+        type: DataTypes.DATE,
+        allowNull: true,
         validate: {
           isDate: true
         }
       },
       birthCity: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       phone: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       citizenship: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       country: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       address: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       zipCode: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       city: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       state: {
-				type: DataTypes.STRING,
-				allowNull: true
+        type: DataTypes.STRING,
+        allowNull: true
       },
       optIn: {
-				type: DataTypes.BOOLEAN,
-				defaultValue: false
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
       },
       passwordForgotten: {
-				type: DataTypes.BOOLEAN,
-				defaultValue: false
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
       },
       passwordForgottenDate: {
-				type: DataTypes.DATE,
-				allowNull: true,
+        type: DataTypes.DATE,
+        allowNull: true,
         validate: {
           isDate: true
         }
       },
-		},
-		{}
-	);
-	User.associate = models => {
-		User.belongsToMany(models.Role, {
-			through: 'UserRoles'
-		});
-	};
-	return User;
-};
+    },
+    {}
+    );
+    User.associate = models => {
+      User.belongsToMany(models.Role, {
+        through: 'UserRoles'
+      });
+    };
+    return User;
+  };
